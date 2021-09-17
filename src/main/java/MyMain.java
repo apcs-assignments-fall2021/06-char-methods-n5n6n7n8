42b4cd528f928a9b22303cb73a530cbd7fff4f58
natewong <nwwong17@gmail.com>
2021-09-16 17:19:44 -0700

    Created char methods for APCS HWW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,49 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        int chn = ch;
+        if(ch>='A'&&ch<='Z')
+        {
+            chn+=32;
+            ch = (char) chn;
+            return ch;
+        }
+        return ch;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        int chn = ch;
+        if(ch>='a'&&ch<='z')
+        {
+            chn-=32;
+            ch = (char) chn;
+            return ch;
+        }
+        return ch;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String ans = "";
+        for(int i=0; i<str.length(); i++)
+        {
+            if(!(str.charAt(i)>='0'&&str.charAt(i)<='9'))
+            {
+                ans+=str.charAt(i);
+            }
+        }
+        return ans;
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(toLower('F'));
+        System.out.println(toUpper('b'));
+        System.out.println(removeNumbers("h9340e93240ll4243o4!"));
     }
 }
